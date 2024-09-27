66e61a14d321f3f3cfb2799d914c74c502d1189e
heesane <tees3359@gmail.com>
2024-09-27 22:25:47 +0900

    Feat : Springboot Setting

---
+++ jamanchu/src/main/java/com/recipe/jamanchu/JamanchuApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.jamanchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamanchuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamanchuApplication.class, args);
+	}
+
+}
