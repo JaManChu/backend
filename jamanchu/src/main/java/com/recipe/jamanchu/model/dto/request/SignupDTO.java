a0840853237f6cf0b65c5b9b065990f60e6d7cfd
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-09-28 16:08:16 +0900

    Refactor : SignupDTO Setter 어노테이션 삭제

--- jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/SignupDTO.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/SignupDTO.java
@@ -2,10 +2,8 @@ package com.recipe.jamanchu.model.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class SignupDTO {
 
