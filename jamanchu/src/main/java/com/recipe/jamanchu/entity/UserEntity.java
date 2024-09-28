45b2ada9a6389505de60175090b6ee4d652675f8
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-09-28 16:37:50 +0900

    Refactor : 사용하지 않은 import 구문 제거

--- jamanchu/src/main/java/com/recipe/jamanchu/entity/UserEntity.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/entity/UserEntity.java
@@ -9,7 +9,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
