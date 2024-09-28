2ae6f5fbf0d336736b00faf8fe11e95d80d99913
heesane <tees3359@gmail.com>
2024-09-28 21:05:18 +0900

    Feat : Ingredient Rating Entity & Entity Builder Test

---
+++ jamanchu/src/test/java/com/recipe/jamanchu/entity/IngredientRatingEntityTest.java
@@ -0,0 +1,58 @@
+package com.recipe.jamanchu.entity;
+
+import static com.recipe.jamanchu.model.type.UserRole.USER;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import com.recipe.jamanchu.model.type.UserRole;
+import com.recipe.jamanchu.repository.IngredientRatingRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class IngredientRatingEntityTest {
+
+  @Mock
+  private IngredientRatingRepository ingredientRatingRepository;
+
+  @Test
+  @DisplayName("Ingredient Rating Entity Builder Test")
+  void builder() {
+
+    // given
+    UserEntity ratingUser = UserEntity.builder()
+        .userId(1L)
+        .nickname("test")
+        .email("test@gmail.com")
+        .password("test")
+        .role(USER)
+        .build();
+
+    IngredientEntity ingredient = IngredientEntity.builder()
+        .ingredientId(1L)
+        .name("ingredient")
+        .build();
+
+    IngredientRatingEntity ingredientRatingEntity = IngredientRatingEntity.builder()
+        .user(ratingUser)
+        .ingredient(ingredient)
+        .rating(5.0)
+        .point(5.0)
+        .build();
+
+    // when
+    when(ingredientRatingRepository.save(ingredientRatingEntity)).thenReturn(ingredientRatingEntity);
+
+    // act
+    IngredientRatingEntity result = ingredientRatingRepository.save(ingredientRatingEntity);
+
+    // then
+    assertEquals(ingredientRatingEntity.getIngredient().getIngredientId(), result.getIngredient().getIngredientId());
+    assertEquals(ingredientRatingEntity.getUser().getUserId(), result.getUser().getUserId());
+    assertEquals(ingredientRatingEntity.getRating(), result.getRating());
+    assertEquals(ingredientRatingEntity.getPoint(), result.getPoint());
+  }
+}
