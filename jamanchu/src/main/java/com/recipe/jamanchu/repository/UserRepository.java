2bf2acdaf8d6f9e750a09d8ba06a2b74cc906b61
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-09-28 16:52:21 +0900

    Refactor : UserRepository findByEmail 메서드 삭제

--- jamanchu/src/main/java/com/recipe/jamanchu/repository/UserRepository.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.recipe.jamanchu.repository;
 
 import com.recipe.jamanchu.entity.UserEntity;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
   boolean existsByEmail(String email);
-
-  Optional<UserEntity> findByEmail(String email);
 
   boolean existsByNickname(String nickname);
 }
