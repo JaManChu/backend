32797f77acb8177511d53e225254edcc5cc52e68
heesane <tees3359@gmail.com>
2024-09-28 21:07:01 +0900

    Feat : Statistics Entity & Entity Builder Test

---
+++ jamanchu/src/main/java/com/recipe/jamanchu/entity/StatisticsEntity.java
@@ -0,0 +1,38 @@
+package com.recipe.jamanchu.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "statistics")
+public class StatisticsEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "stat_id")
+  private Long analyticsId;
+
+  @NotNull
+  @Column(name = "stat_days")
+  private Long days;
+
+  @NotNull
+  @Column(name = "stat_months")
+  private Long months;
+
+  @NotNull
+  @Column(name = "stat_years")
+  private Long years;
+
+}
