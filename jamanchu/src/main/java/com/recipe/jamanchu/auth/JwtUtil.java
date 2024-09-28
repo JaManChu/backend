3bd696279eafc9eacd9c0e18a28ac80bd86f5e32
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-09-28 19:57:23 +0900

    Feat : JWT JwtUtil 클래스 구현

---
+++ jamanchu/src/main/java/com/recipe/jamanchu/auth/JwtUtil.java
@@ -0,0 +1,69 @@
+package com.recipe.jamanchu.auth;
+
+import com.recipe.jamanchu.model.type.UserRole;
+import io.jsonwebtoken.Jwts;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+  private final SecretKey secretKey;
+  public static final long ACCESS_TOKEN_EXPIRE_TIME = 10 * 60 * 1000L;  // 10분
+  public static final long REFRESH_TOKEN_EXPIRE_TIME = 24 * 60 * 60 * 1000L;  // 24시간
+
+  public JwtUtil(@Value("${spring.jwt.secret.key}")String secret) {
+
+    secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+        Jwts.SIG.HS256.key().build().getAlgorithm());
+  }
+
+  public Long getUserId(String token) {
+    return Jwts.parser().verifyWith(secretKey).build()
+        .parseSignedClaims(token)
+        .getPayload()
+        .get("userId", Long.class);
+  }
+
+  public UserRole getRole(String token) {
+    return Jwts.parser().verifyWith(secretKey).build()
+        .parseSignedClaims(token)
+        .getPayload()
+        .get("role", UserRole.class);
+  }
+
+  public String getType(String token) {
+    return Jwts.parser().verifyWith(secretKey).build()
+        .parseSignedClaims(token)
+        .getPayload()
+        .get("type", String.class);
+  }
+
+  public void isExpired(String token) {
+    Jwts.parser().verifyWith(secretKey).build()
+        .parseSignedClaims(token)
+        .getPayload()
+        .getExpiration();
+  }
+
+
+  public String createJwt(String type, Long userId, UserRole role) {
+    long expirationTime = "access".equals(type)
+        ? ACCESS_TOKEN_EXPIRE_TIME
+        : REFRESH_TOKEN_EXPIRE_TIME;
+
+    return Jwts.builder()
+        .claim("type", type)
+        .claim("userId", userId)
+        .claim("role", role)
+        .notBefore(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + expirationTime))
+        .signWith(secretKey)
+        .compact();
+  }
+}
+
