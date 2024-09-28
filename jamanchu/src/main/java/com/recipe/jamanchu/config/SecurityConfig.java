1448651a0826ed462d8f1496c10034fba312b50a
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-09-28 19:55:46 +0900

    Refactor : Spring Security 로그인 로직 추가

--- jamanchu/src/main/java/com/recipe/jamanchu/config/SecurityConfig.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/config/SecurityConfig.java
@@ -1,33 +1,59 @@
 package com.recipe.jamanchu.config;
 
+import com.recipe.jamanchu.auth.JwtFilter;
+import com.recipe.jamanchu.auth.JwtUtil;
+import com.recipe.jamanchu.auth.LoginFilter;
+import com.recipe.jamanchu.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
+  private final AuthenticationConfiguration authenticationConfiguration;
   private final BCryptPasswordEncoder passwordEncoder;
+  private final JwtUtil jwtUtil;
+  private final UserRepository userRepository;
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+    return configuration.getAuthenticationManager();
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     http
         .csrf(AbstractHttpConfigurer::disable);
-
     http
         .httpBasic(AbstractHttpConfigurer::disable);
-
     http
         .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll());
+            .requestMatchers("/", "/api/v1/user/signup", "login").permitAll()
+            .anyRequest().authenticated()
+        );
+
+    http
+        .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+    http
+        .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)
+            ,passwordEncoder, jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class);
+    http
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
 
