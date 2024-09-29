6fb052f18f99534d8040bdbf85709a797c2dcd05
heesane <tees3359@gmail.com>
2024-09-29 23:46:53 +0900

    Feat : CommentsUpdateDTO Test & Custom Annotation

--- jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsUpdateDTO.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsUpdateDTO.java
@@ -12,16 +12,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CommentsUpdateDTO {
 
-  @NotEmpty
-  @Min(1)
+  @Min(value = 1, message = "잘못된 댓글 번호입니다.")
   private final Long commentsId;
 
-  @NotEmpty
-  @Size(min = 1, max = 300)
+  @NotEmpty(message = "댓글 내용을 입력해주세요.")
+  @Size(min = 1, max = 300, message = "댓글 내용은 1자 이상 300자 이하로 입력해주세요.")
   private final String comment;
 
-  @DecimalMin(value = "1.0", inclusive = false)
-  @DecimalMax(value = "5.0", inclusive = false)
+  @DecimalMin(value = "1.0", inclusive = false, message = "평점은 1.0이상으로 입력해주세요.")
+  @DecimalMax(value = "5.0", inclusive = false, message = "평점은 5.0이하로 입력해주세요.")
   private final Double rating;
 
 }
