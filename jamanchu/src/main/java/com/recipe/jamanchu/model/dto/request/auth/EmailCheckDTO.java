94549f6d00039b12db2d3d0661593e4026e3b03d
heesane <tees3359@gmail.com>
2024-09-29 21:29:04 +0900

    Feat : Email Check DTO Test

--- jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/auth/EmailCheckDTO.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/auth/EmailCheckDTO.java
@@ -1,5 +1,7 @@
 package com.recipe.jamanchu.model.dto.request.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,5 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class EmailCheckDTO {
 
+  @NotEmpty(message = "이메일을 입력해주세요.")
+  @Email(message = "이메일 형식이 아닙니다.")
   private final String checkingEmail;
 }
