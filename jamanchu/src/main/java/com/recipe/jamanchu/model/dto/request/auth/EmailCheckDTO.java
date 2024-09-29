8a33c93e45474692e8d53b95377492bd2b7f097d
heesane <tees3359@gmail.com>
2024-09-29 21:11:58 +0900

    Feat : Email Check DTO

---
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/auth/EmailCheckDTO.java
@@ -0,0 +1,11 @@
+package com.recipe.jamanchu.model.dto.request.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EmailCheckDTO {
+
+  private final String checkingEmail;
+}
