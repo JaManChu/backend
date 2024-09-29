6dbe6e96d1197dd81e2c610a8a6644afc55389cb
heesane <tees3359@gmail.com>
2024-09-29 23:46:37 +0900

    Feat : CommentsDeleteDTO Test & Custom Annotation

--- jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsDeleteDTO.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsDeleteDTO.java
@@ -1,5 +1,6 @@
 package com.recipe.jamanchu.model.dto.request.comments;
 
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,5 +8,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CommentsDeleteDTO {
 
+    @Min(value = 1, message = "잘못된 입력입니다.")
     private final Long commentId;
 }
