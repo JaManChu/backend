11c193cb775bdd2c5ccd5982399410d16ce49ab7
heesane <tees3359@gmail.com>
2024-09-30 18:00:50 +0900

    Fix : NotEmpty에서 NotNull로 변경

--- jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsDTO.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsDTO.java
@@ -3,7 +3,7 @@ package com.recipe.jamanchu.model.dto.request.comments;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +15,7 @@ public class CommentsDTO {
   @Min(value = 1, message = "잘못된 레시피 ID 입력입니다.")
   private final Long recipeId;
 
-  @NotEmpty(message = "댓글을 입력해주세요.")
+  @NotNull(message = "댓글을 입력해주세요.")
   @Size(min = 1, max = 300, message = "댓글은 1자 이상 300자 이하로 입력해주세요.")
   private final String comment;
 
--- jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsUpdateDTO.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsUpdateDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +16,7 @@ public class CommentsUpdateDTO {
   @Min(value = 1, message = "잘못된 댓글 번호입니다.")
   private final Long commentsId;
 
-  @NotEmpty(message = "댓글 내용을 입력해주세요.")
+  @NotNull(message = "댓글 내용을 입력해주세요.")
   @Size(min = 1, max = 300, message = "댓글 내용은 1자 이상 300자 이하로 입력해주세요.")
   private final String comment;
 
