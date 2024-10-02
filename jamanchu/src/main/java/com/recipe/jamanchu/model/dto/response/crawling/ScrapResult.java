edf158a9fcd18525ccf4440f174098b0c1eb7205
jungheol <jungheol@naver.com>
2024-10-02 16:47:34 +0900

    Feat: ScrapResult Dto 추가

---
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/response/crawling/ScrapResult.java
@@ -0,0 +1,23 @@
+package com.recipe.jamanchu.model.dto.response.crawling;
+
+import com.recipe.jamanchu.model.type.CookingTimeType;
+import com.recipe.jamanchu.model.type.LevelType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class ScrapResult {
+
+  private String title;
+  private String authorName;
+  private String description;
+  private LevelType levelType;
+  private CookingTimeType cookTime;
+  private String thumbnail;
+  private Double rating;
+  private String ingredients;
+  private String manualContents;
+  private String manualPictures;
+}
