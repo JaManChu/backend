503f539ef49abb8a4ba11c95fd561531c74a6d62
heesane <tees3359@gmail.com>
2024-10-03 18:11:01 +0900

    Feat : Comments Repository

--- jamanchu/src/main/java/com/recipe/jamanchu/repository/CommentRepository.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/repository/CommentRepository.java
@@ -1,10 +1,12 @@
 package com.recipe.jamanchu.repository;
 
 import com.recipe.jamanchu.entity.CommentEntity;
+import com.recipe.jamanchu.entity.RecipeEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-
+  List<CommentEntity> findAllByRecipe(RecipeEntity recipe);
 }
