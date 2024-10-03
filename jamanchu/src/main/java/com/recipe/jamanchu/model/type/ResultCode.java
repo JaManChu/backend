45807e2dae8dc29fb791f653af38008cdf93510e
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-10-03 17:27:45 +0900

    Refactor : SUCCESS_UPDATE_USER_INFO 코드 추가

--- jamanchu/src/main/java/com/recipe/jamanchu/model/type/ResultCode.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/type/ResultCode.java
@@ -11,6 +11,7 @@ public enum ResultCode {
   SUCCESS_SIGNUP(HttpStatus.CREATED, "회원가입 성공!"),
   SUCCESS_COMMENTS(HttpStatus.CREATED, "댓글 작성 성공!"),
   SUCCESS_CR_RECIPE(HttpStatus.CREATED, "스크래핑 레시피 저장 성공!"),
+  SUCCESS_UPDATE_USER_INFO(HttpStatus.OK, "회원 정보 수정 성공!"),
   ;
 
   private final HttpStatus statusCode;
