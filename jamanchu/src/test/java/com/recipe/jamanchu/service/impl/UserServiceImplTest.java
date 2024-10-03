e6e0d4f64d091597492ff799b1d93b77b5965d3f
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-10-03 17:25:05 +0900

    Test : 회원정보 수정 테스트 코드 구현

--- jamanchu/src/test/java/com/recipe/jamanchu/service/impl/UserServiceImplTest.java
+++ jamanchu/src/test/java/com/recipe/jamanchu/service/impl/UserServiceImplTest.java
@@ -2,18 +2,27 @@ package com.recipe.jamanchu.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
+import com.recipe.jamanchu.entity.UserEntity;
 import com.recipe.jamanchu.exceptions.exception.DuplicatedEmailException;
 import com.recipe.jamanchu.exceptions.exception.DuplicatedNicknameException;
+import com.recipe.jamanchu.exceptions.exception.SocialAccountException;
 import com.recipe.jamanchu.model.dto.request.auth.SignupDTO;
+import com.recipe.jamanchu.model.dto.request.auth.UserUpdateDTO;
 import com.recipe.jamanchu.model.type.ResultCode;
+import com.recipe.jamanchu.model.type.UserRole;
 import com.recipe.jamanchu.repository.UserRepository;
+import com.recipe.jamanchu.validator.ValidateUser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -28,6 +37,9 @@ class UserServiceImplTest {
 
   @InjectMocks
   private UserServiceImpl userServiceimpl;
+
+  @Mock
+  private ValidateUser validateUser;
 
   @Test
   @DisplayName("회원가입 성공")
@@ -74,5 +86,51 @@ class UserServiceImplTest {
 
     // when then
     assertThrows(DuplicatedNicknameException.class, () -> userServiceimpl.signup(newUser));
+  }
+
+  @Test
+  @DisplayName("회원정보 수정 성공")
+  void updateUserInfo_Success() {
+    // given
+    UserUpdateDTO userUpdateDTO = new UserUpdateDTO(1L, "newPassword", "newNickName");
+
+    UserEntity userEntity = UserEntity.builder()
+        .userId(1L)
+        .email("test@example.com")
+        .nickname("TestUser")
+        .role(UserRole.USER)
+        .password("password")
+        .provider(null)
+        .build();
+
+    when(validateUser.validateUserId(1L)).thenReturn(userEntity);
+    when(passwordEncoder.encode("newPassword")).thenReturn("encodedNewPassword");
+
+    // 테스트 실행
+    ResultCode result = userServiceimpl.updateUserInfo(userUpdateDTO);
+
+    // 검증
+    assertEquals(ResultCode.SUCCESS_UPDATE_USER_INFO, result);
+  }
+
+  @Test
+  @DisplayName("회원정보 수정 실패 : OAuth 회원가입을 한 회원")
+  void updateUserInfo_SocialAccountException() {
+    // given
+    UserUpdateDTO userUpdateDTO = new UserUpdateDTO(1L, "newPassword", "newNickName");
+
+    UserEntity userEntity = UserEntity.builder()
+        .userId(1L)
+        .email("test@example.com")
+        .nickname("TestUser")
+        .role(UserRole.USER)
+        .password("password")
+        .provider("KAKAO")
+        .build();
+
+    when(validateUser.validateUserId(1L)).thenReturn(userEntity);
+
+    // when then
+    assertThrows(SocialAccountException.class, () -> userServiceimpl.updateUserInfo(userUpdateDTO));
   }
 }
