4c05917b6b63c3e9e93d767b40b1d97b14982bac
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-10-05 17:04:32 +0900

    Refactor : 코드 수정

--- jamanchu/src/main/java/com/recipe/jamanchu/config/WebMvcConfig.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/config/WebMvcConfig.java
@@ -22,6 +22,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         .allowedMethods(ALLOW_METHOD_NAMES.split(","))  // 허용할 HTTP Method 목록
         .allowedHeaders("*")        // 모든 HTTP header 허용
         .allowCredentials(true)     // 자격 증명 허용
-        .exposedHeaders(HttpHeaders.LOCATION, HttpHeaders.AUTHORIZATION, "Refresh-Token");  // 클라이언트에 노출할 헤더 목록
+        .exposedHeaders("access-token", HttpHeaders.LOCATION);   // 클라이언트에 노출할 헤더 목록
   }
 }
