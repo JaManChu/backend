67b080c55bdd9971e8dba430c3f01463085151c6
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-10-08 00:28:28 +0900

    Feat : ResultCode 회원정보 조회 성공 상태 코드 추가

--- jamanchu/src/main/java/com/recipe/jamanchu/model/type/ResultCode.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/type/ResultCode.java
@@ -16,7 +16,8 @@ public enum ResultCode {
   SUCCESS_CR_RECIPE(HttpStatus.CREATED, "스크래핑 레시피 저장 성공!"),
   SUCCESS_UPDATE_USER_INFO(HttpStatus.OK, "회원 정보 수정 성공!"),
   SUCCESS_INSERT_CR_DATA(HttpStatus.CREATED, "데이터 분산 저장 성공!"),
-  SUCCESS_DELETE_USER(HttpStatus.OK, "회원 탈퇴 성공!")
+  SUCCESS_DELETE_USER(HttpStatus.OK, "회원 탈퇴 성공!"),
+  SUCCESS_GET_USER_INFO(HttpStatus.OK, "회원 정보 조회 성공!")
   ;
 
   private final HttpStatus statusCode;
