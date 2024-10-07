73546c67234f886f1e09b3d9cc56c9771e91572b
jungheol <jungheol@naver.com>
2024-10-07 19:45:12 +0900

    Feat : 가장 최신 recipeId 검색하는 쿼리 추가

--- jamanchu/src/main/java/com/recipe/jamanchu/repository/TenThousandRecipeRepository.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/repository/TenThousandRecipeRepository.java
@@ -3,9 +3,13 @@ package com.recipe.jamanchu.repository;
 import com.recipe.jamanchu.entity.TenThousandRecipeEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TenThousandRecipeRepository extends JpaRepository<TenThousandRecipeEntity, Long> {
-  List<TenThousandRecipeEntity> findByCrawledRecipeIdBetween(Long startId, Long endId);
+  List<TenThousandRecipeEntity> findByRecipeIdBetween(Long startId, Long endId);
+
+  @Query("SELECT MAX(t.recipeId) FROM TenThousandRecipeEntity t")
+  Long findMaxRecipeId();
 }
