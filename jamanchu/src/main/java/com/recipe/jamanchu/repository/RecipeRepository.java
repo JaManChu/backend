dbb1d7cabde328112a5b8abb8fc9a9700ca6b451
jungheol <jungheol@naver.com>
2024-10-09 22:21:17 +0900

    Feat : and 와 or 조건 검색 쿼리 추가

--- jamanchu/src/main/java/com/recipe/jamanchu/repository/RecipeRepository.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/repository/RecipeRepository.java
@@ -1,10 +1,38 @@
 package com.recipe.jamanchu.repository;
 
 import com.recipe.jamanchu.entity.RecipeEntity;
+import com.recipe.jamanchu.model.type.CookingTimeType;
+import com.recipe.jamanchu.model.type.LevelType;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
 
+  @Query(value = "SELECT r FROM RecipeEntity r JOIN r.ingredients ri " +
+      "WHERE (:ingredients IS NULL OR ri.name IN :ingredients) " +
+      "AND (:cookingTime IS NULL OR r.time = :cookingTime) " +
+      "AND (:level IS NULL OR r.level = :level) " +
+      "GROUP BY r.id " +
+      "HAVING COUNT(ri.name) = :ingredientCount")
+  Page<RecipeEntity> searchAndRecipes(@Param("level") LevelType level,
+      @Param("cookingTime") CookingTimeType cookingTime,
+      @Param("ingredients") List<String> ingredients,
+      @Param("ingredientCount") Long ingredientCount,
+      Pageable pageable);
+
+  @Query(value = "SELECT r FROM RecipeEntity r JOIN r.ingredients ri " +
+      "WHERE (:level IS NULL OR r.level = :level) " +
+      "OR (:cookingTime IS NULL OR r.time = :cookingTime) " +
+      "OR (:ingredients IS NULL OR ri.name IN :ingredients) " +
+      "GROUP BY r.id")
+  Page<RecipeEntity> searchOrRecipes(@Param("level") LevelType level,
+      @Param("cookingTime") CookingTimeType cookingTime,
+      @Param("ingredients") List<String> ingredients,
+      Pageable pageable);
 }
