ceb78338c7ee8b4603f7fe470b9de3d6373199c8
heesane <tees3359@gmail.com>
2024-10-09 19:38:51 +0900

    Refactor : Swagger Config 수정

--- jamanchu/src/main/java/com/recipe/jamanchu/config/SwaggerConfig.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +17,10 @@ public class SwaggerConfig {
         .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
         .components(new Components()
             .addSecuritySchemes("bearerAuth",
-                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
+                new SecurityScheme()
+                    .name("access-token")
+                    .type(Type.APIKEY)
+                    .in(SecurityScheme.In.HEADER)
                     .bearerFormat("JWT")))
         .info(new Info()
             .title("JaManChu API")
