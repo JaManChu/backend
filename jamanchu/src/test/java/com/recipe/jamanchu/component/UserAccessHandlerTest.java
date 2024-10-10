a2fa17d8067b710880cf3b9e11be23e9852de422
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-10-11 04:54:34 +0900

    Test : 변경된 로직에 맞게 테스트 코드 수정

--- jamanchu/src/test/java/com/recipe/jamanchu/component/UserAccessHandlerTest.java
+++ jamanchu/src/test/java/com/recipe/jamanchu/component/UserAccessHandlerTest.java
@@ -12,6 +12,7 @@ import com.recipe.jamanchu.exceptions.exception.DuplicatedNicknameException;
 import com.recipe.jamanchu.exceptions.exception.PasswordMismatchException;
 import com.recipe.jamanchu.exceptions.exception.SocialAccountException;
 import com.recipe.jamanchu.exceptions.exception.UserNotFoundException;
+import com.recipe.jamanchu.model.type.ResultCode;
 import com.recipe.jamanchu.repository.UserRepository;
 import java.util.HashMap;
 import java.util.Map;
@@ -155,25 +156,53 @@ class UserAccessHandlerTest {
   }
 
   @Test
-  @DisplayName("existsByEmail - 실패: 이메일 중복")
+  @DisplayName("existsById - 성공 : 사용자 존재")
+  void existsById_Success() {
+    // given
+    Long userId = 1L;
+    when(userRepository.existsById(userId)).thenReturn(true);
+
+    // when & then
+    assertDoesNotThrow(() -> userAccessHandler.existsById(userId));
+  }
+
+  @Test
+  @DisplayName("existsById - 실패 : 존재하지 않은 사용자")
+  void existsById_UserNotFound() {
+    // given
+    Long userId = 1L;
+    when(userRepository.existsById(userId)).thenReturn(false);
+
+    // when & then
+    assertThrows(UserNotFoundException.class, () -> userAccessHandler.existsById(userId));
+  }
+
+  @Test
+  @DisplayName("existsByEmail :  이미 사용 중인 이메일입니다.")
   void existsByEmail_Duplicated() {
     // given
     String email = "test@example.com";
     when(userRepository.existsByEmail(email)).thenReturn(true);
 
-    // when & then
-    assertThrows(DuplicatedEmailException.class, () -> userAccessHandler.existsByEmail(email));
+    // when
+    ResultCode resultCode = userAccessHandler.existsByEmail(email);
+
+    // then
+    assertEquals(ResultCode.EMAIL_ALREADY_IN_USE, resultCode);
   }
 
   @Test
-  @DisplayName("existsByEmail - 성공: 이메일 미중복")
+  @DisplayName("existsByEmail : 사용할 수 있는 이메일입니다.")
   void existsByEmail_NotDuplicated() {
     // given
-    String email = "unique@example.com";
+    String email = "test@example.com";
     when(userRepository.existsByEmail(email)).thenReturn(false);
 
-    // when & then
-    assertDoesNotThrow(() -> userAccessHandler.existsByEmail(email));
+    // when
+    ResultCode resultCode = userAccessHandler.existsByEmail(email);
+
+    // then
+    assertEquals(ResultCode.EMAIL_AVAILABLE, resultCode);
   }
 
   @Test
@@ -256,12 +285,13 @@ class UserAccessHandlerTest {
         .role(com.recipe.jamanchu.model.type.UserRole.USER)
         .build();
 
-    when(userRepository.save(newUser)).thenReturn(newUser);
+    when(userRepository.save(user)).thenReturn(newUser);
 
     // when
-    userAccessHandler.saveUser(newUser);
+    userAccessHandler.saveUser(user);
 
     // then
-    verify(userRepository).save(newUser);
+    verify(userRepository).save(user);
   }
+
 }
