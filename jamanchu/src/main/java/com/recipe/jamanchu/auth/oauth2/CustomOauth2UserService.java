401af36139a0e4b0b5540e8e7b149dae5ac62785
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-10-12 14:37:49 +0900

    feat : 카카오 측에 Access-token, 사용자 정보를 요청하는 메서드 구현

--- jamanchu/src/main/java/com/recipe/jamanchu/auth/oauth2/CustomOauth2UserService.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/auth/oauth2/CustomOauth2UserService.java
@@ -1,24 +1,87 @@
 package com.recipe.jamanchu.auth.oauth2;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+public class CustomOauth2UserService  {
 
-  @Override
-  public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+  @Value("${PROD.AUTH.OAUTH.REGISTRATION.KAKAO.client-id}")
+  private String clientId;
 
-    log.info("loadUser -> userRequest : {}", userRequest);
-    return new DefaultOAuth2UserService().loadUser(userRequest);
+  @Value("${PROD.AUTH.OAUTH.REGISTRATION.KAKAO.client-secret}")
+  private String clientSecretId;
+
+  @Value("${PROD.AUTH.OAUTH.REGISTRATION.KAKAO.redirect-uri}")
+  private String redirectUri;
+
+  public String getAccessToken(String code) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+    // HTTP Body 생성
+    MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+    body.add("grant_type", "authorization_code");
+    body.add("client_id", clientId);
+    body.add("client_secret", clientSecretId);
+    body.add("redirect_uri", redirectUri);
+    body.add("code", code);
+
+    // HTTP 요청 보내기
+    HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(body, headers);
+    RestTemplate rt = new RestTemplate();
+    ResponseEntity<Map> response = rt.exchange(
+        "https://kauth.kakao.com/oauth/token",
+        HttpMethod.POST,
+        kakaoTokenRequest,
+        Map.class
+    );
+
+    // Access-token 반환
+    if (response.getStatusCode() == HttpStatus.OK) {
+      Map responseBody = response.getBody();
+      if (responseBody != null && responseBody.containsKey("access_token")) {
+        return responseBody.get("access_token").toString();
+      }
+    }
+    throw new RuntimeException("액세스 토큰을 가져오는 데 실패했습니다.");
   }
 
+  // 사용자 정보 가지고 오기
+  public KakaoUserDetails getUserDetails(String accessToken) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Authorization", "Bearer " + accessToken);
+    headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+    // HTTP 요청 보내기
+    HttpEntity<MultiValueMap<String, String>> kakaoUserInfoRequest = new HttpEntity<>(headers);
+    RestTemplate rt = new RestTemplate();
+    ResponseEntity<Map> response = rt.exchange(
+        "https://kapi.kakao.com/v2/user/me",
+        HttpMethod.POST,
+        kakaoUserInfoRequest,
+        Map.class
+    );
+
+    if (response.getStatusCode() == HttpStatus.OK) {
+      if (response.getBody() != null) {
+        return new KakaoUserDetails(response.getBody());
+      }
+    }
+    throw new RuntimeException("사용자 정보를 가져오는 데 실패했습니다.");
+  }
 }
+
