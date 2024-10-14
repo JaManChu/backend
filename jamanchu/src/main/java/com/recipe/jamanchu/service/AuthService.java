ccd51126189b2361141e796827691dab0203eb94
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-10-15 02:46:27 +0900

    Refactor : 반환값 타입 수정

--- jamanchu/src/main/java/com/recipe/jamanchu/service/AuthService.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/service/AuthService.java
@@ -1,10 +1,10 @@
 package com.recipe.jamanchu.service;
 
-import com.recipe.jamanchu.model.type.ResultCode;
+import com.recipe.jamanchu.model.dto.response.ResultResponse;
 
 public interface AuthService {
 
-  ResultCode checkEmail(String email);
+  ResultResponse checkEmail(String email);
 
-  ResultCode checkNickname(String nickname);
+  ResultResponse checkNickname(String nickname);
 }
--- jamanchu/src/main/java/com/recipe/jamanchu/service/impl/AuthServiceImpl.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/service/impl/AuthServiceImpl.java
@@ -1,7 +1,7 @@
 package com.recipe.jamanchu.service.impl;
 
 import com.recipe.jamanchu.component.UserAccessHandler;
-import com.recipe.jamanchu.model.type.ResultCode;
+import com.recipe.jamanchu.model.dto.response.ResultResponse;
 import com.recipe.jamanchu.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,13 +13,14 @@ public class AuthServiceImpl implements AuthService {
   private final UserAccessHandler userAccessHandler;
 
   @Override
-  public ResultCode checkEmail(String email) {
+  public ResultResponse checkEmail(String email) {
 
     return userAccessHandler.existsByEmail(email);
   }
 
   @Override
-  public ResultCode checkNickname(String nickname) {
+  public ResultResponse checkNickname(String nickname) {
+
     return userAccessHandler.existsByNickname(nickname);
   }
 }
