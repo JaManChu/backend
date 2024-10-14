f9a3cf9b266c8e9a5978692e3b587e5d834015e8
heesane <tees3359@gmail.com>
2024-10-14 16:46:34 +0900

    Refactor : Controller 주석 추가

--- jamanchu/src/main/java/com/recipe/jamanchu/notify/NotifyController.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/notify/NotifyController.java
@@ -18,8 +18,17 @@ public class NotifyController {
 
   private final NotifyServiceImpl notifyService;
 
+  // 토큰 기반 알림 전체 연결
   @GetMapping(value = "/notify", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   public SseEmitter getNotifications(HttpServletRequest request) {
     return notifyService.subscribe(request);
   }
+
+
+  // 개발 중인 API
+  // 특정 레시피에 대한 알림을 받지 않는다.
+//  @PostMapping("/notify/ignore/{recipeId}")
+//  public ResponseEntity<ResultResponse> ignoreRecipeCommentAlarm(HttpServletRequest request, @PathVariable("recipeId") Long recipeId) {
+//    return ResponseEntity.ok(notifyService.ignoreSRecipeCommentAlarm(request));
+//  }
 }
