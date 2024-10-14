f6817d79fa72d12175410b4f66c4cbfb7d1868ce
jungheol <jungheol@naver.com>
2024-10-14 23:00:00 +0900

    Feat : userId 와 recipeId로 찜레시피 있는지 검색하는 쿼리 추가

--- jamanchu/src/main/java/com/recipe/jamanchu/repository/ScrapedRecipeRepository.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/repository/ScrapedRecipeRepository.java
@@ -1,10 +1,13 @@
 package com.recipe.jamanchu.repository;
 
+import com.recipe.jamanchu.entity.RecipeEntity;
 import com.recipe.jamanchu.entity.ScrapedRecipeEntity;
+import com.recipe.jamanchu.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScrapedRecipeRepository extends JpaRepository<ScrapedRecipeEntity, Long> {
 
+  ScrapedRecipeEntity findByUserAndRecipe(UserEntity user, RecipeEntity recipe);
 }
