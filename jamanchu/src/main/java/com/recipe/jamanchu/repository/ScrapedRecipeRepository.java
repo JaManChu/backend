f5be2d9cb2bb9f06dab9e61fe3af2dd0d0abddb1
jungheol <jungheol@naver.com>
2024-10-15 17:57:34 +0900

    Feat : 스크랩 레시피 테이블에서 해당 유저가 저장한 recipeId 리스트 가져오는 쿼리 추가

--- jamanchu/src/main/java/com/recipe/jamanchu/repository/ScrapedRecipeRepository.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/repository/ScrapedRecipeRepository.java
@@ -3,11 +3,17 @@ package com.recipe.jamanchu.repository;
 import com.recipe.jamanchu.entity.RecipeEntity;
 import com.recipe.jamanchu.entity.ScrapedRecipeEntity;
 import com.recipe.jamanchu.entity.UserEntity;
+import com.recipe.jamanchu.model.type.ScrapedType;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScrapedRecipeRepository extends JpaRepository<ScrapedRecipeEntity, Long> {
 
   ScrapedRecipeEntity findByUserAndRecipe(UserEntity user, RecipeEntity recipe);
+
+  @Query("SELECT sr.recipe.id FROM ScrapedRecipeEntity sr WHERE sr.user.userId = :userId AND sr.scrapedType = :scrapedType")
+  List<Long> findRecipeIdsByUserIdAndScrapedType(Long userId, ScrapedType scrapedType);
 }
