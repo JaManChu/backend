017fb679c4b4f222f2d842c83cd3cf941a14a02c
heesane <tees3359@gmail.com>
2024-10-15 21:06:36 +0900

    refactor : remove unused import

--- jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsUpdateDTO.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/request/comments/CommentsUpdateDTO.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
