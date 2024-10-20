4f1eb608adcc97257d4fa9e06eef080ef5563f5b
heesane <tees3359@gmail.com>
2024-10-20 17:38:24 +0900

    feat : 추천 레시피가 없을 경우, empty 메서드를 통해 전달

--- jamanchu/src/main/java/com/recipe/jamanchu/model/dto/response/recipes/RecommendRecipes.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/model/dto/response/recipes/RecommendRecipes.java
@@ -8,12 +8,20 @@ public class RecommendRecipes {
 
     private final List<RecommendRecipe> recipes;
 
+    private RecommendRecipes() {
+      this.recipes = null;
+    }
+
     private RecommendRecipes(List<RecommendRecipe> recipes) {
       this.recipes = recipes;
     }
 
     public static RecommendRecipes of(List<RecommendRecipe> recipes) {
       return new RecommendRecipes(recipes);
+    }
+
+    public static RecommendRecipes empty(){
+      return new RecommendRecipes();
     }
 
 }
