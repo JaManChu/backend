4869235556d6352776d62e13f974347fddaa58b9
jungheol <jungheol@naver.com>
2024-10-20 20:15:52 +0900

    Feat : 레시피 등록 및 수정할 때 저장된 파일 url 받아서 진행되도록 수정

--- jamanchu/src/main/java/com/recipe/jamanchu/service/RecipeService.java
+++ jamanchu/src/main/java/com/recipe/jamanchu/service/RecipeService.java
@@ -6,14 +6,15 @@ import com.recipe.jamanchu.model.dto.request.recipe.RecipesSearchDTO;
 import com.recipe.jamanchu.model.dto.request.recipe.RecipesUpdateDTO;
 import com.recipe.jamanchu.model.dto.response.ResultResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
 
 public interface RecipeService {
 
   // 레시피 작성 API
-  ResultResponse registerRecipe(HttpServletRequest request, RecipesDTO recipesDTO);
+  ResultResponse registerRecipe(HttpServletRequest request, RecipesDTO recipesDTO, String thumbnail, List<String> OrderImages);
 
   // 레시피 수정 API
-  ResultResponse updateRecipe(HttpServletRequest request, RecipesUpdateDTO recipesUpdateDTO);
+  ResultResponse updateRecipe(HttpServletRequest request, RecipesUpdateDTO recipesUpdateDTO, String thumbnail, List<String> OrderImages);
 
   // 모든 레시피 조회 API
   ResultResponse getRecipes(HttpServletRequest request, int page, int size);
