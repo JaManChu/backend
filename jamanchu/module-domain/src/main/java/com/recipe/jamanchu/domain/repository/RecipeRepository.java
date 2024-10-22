ec871f861be4b458e25b047decc55154050a77e0
jungheol <jungheol@naver.com>
2024-10-22 17:59:09 +0900

    Feat : 사용하지 않는 쿼리 삭제

--- jamanchu/module-domain/src/main/java/com/recipe/jamanchu/domain/repository/RecipeRepository.java
+++ jamanchu/module-domain/src/main/java/com/recipe/jamanchu/domain/repository/RecipeRepository.java
@@ -5,6 +5,7 @@ import com.recipe.jamanchu.domain.entity.UserEntity;
 import com.recipe.jamanchu.domain.model.type.CookingTimeType;
 import com.recipe.jamanchu.domain.model.type.LevelType;
 import com.recipe.jamanchu.domain.model.type.ScrapedType;
+
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
@@ -16,30 +17,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Long>, RecipeRepositoryCustom {
-
-  // AND 조건으로 검색하는 쿼리
-  @Query(value = "SELECT r FROM RecipeEntity r JOIN r.ingredients ri " +
-      "WHERE (:ingredients IS NULL OR ri.name IN :ingredients) " +
-      "AND (:cookingTime IS NULL OR r.time = :cookingTime) " +
-      "AND (:level IS NULL OR r.level = :level) " +
-      "GROUP BY r.id " +
-      "HAVING COUNT(ri.name) = :ingredientCount")
-  Page<RecipeEntity> searchAndRecipes(@Param("level") LevelType level,
-      @Param("cookingTime") CookingTimeType cookingTime,
-      @Param("ingredients") List<String> ingredients,
-      @Param("ingredientCount") Long ingredientCount,
-      Pageable pageable);
-
-  // OR 조건으로 검색하는 쿼리
-  @Query(value = "SELECT r FROM RecipeEntity r JOIN r.ingredients ri " +
-      "WHERE (:level IS NULL OR r.level = :level) " +
-      "OR (:cookingTime IS NULL OR r.time = :cookingTime) " +
-      "OR (:ingredients IS NULL OR ri.name IN :ingredients) " +
-      "GROUP BY r.id")
-  Page<RecipeEntity> searchOrRecipes(@Param("level") LevelType level,
-      @Param("cookingTime") CookingTimeType cookingTime,
-      @Param("ingredients") List<String> ingredients,
-      Pageable pageable);
 
   // 평점순으로 레시피 가져오는 쿼리
   @Query("SELECT r FROM RecipeEntity r " +
@@ -68,36 +45,6 @@ public interface RecipeRepository extends JpaRepository<RecipeEntity, Long>, Rec
       "GROUP BY r.id " +
       "ORDER BY AVG(rat.rating) DESC")
   Page<RecipeEntity> findByIdNotInOrderByRating(@Param("ids") List<Long> ids, Pageable pageable);
-
-  // 찜 레시피에 해당하는 ids(레시피)는 제외하고 AND 조건으로 검색하는 쿼리
-  @Query(value = "SELECT r FROM RecipeEntity r JOIN r.ingredients ri " +
-      "WHERE (:ingredients IS NULL OR ri.name IN :ingredients) " +
-      "AND (:cookingTime IS NULL OR r.time = :cookingTime) " +
-      "AND (:level IS NULL OR r.level = :level) " +
-      "AND (r.id NOT IN :ids) " +
-      "GROUP BY r.id " +
-      "HAVING COUNT(ri.name) = :ingredientCount")
-  Page<RecipeEntity> searchAndRecipesIdNotIn(@Param("level") LevelType level,
-      @Param("cookingTime") CookingTimeType cookingTime,
-      @Param("ingredients") List<String> ingredients,
-      @Param("ingredientCount") Long ingredientCount,
-      @Param("ids") List<Long> ids,
-      Pageable pageable);
-
-  // 찜 레시피에 해당하는 ids(레시피)는 제외하고 OR 조건으로 검색하는 쿼리
-  @Query(value = "SELECT r FROM RecipeEntity r JOIN r.ingredients ri " +
-      "WHERE r.id NOT IN :ids " +  // Apply the exclusion of ids separately
-      "AND (" +  // Start grouping the OR conditions
-      "(:level IS NULL OR r.level = :level) " +
-      "OR (:cookingTime IS NULL OR r.time = :cookingTime) " +
-      "OR (:ingredients IS NULL OR ri.name IN :ingredients)" +
-      ") " +  // End grouping the OR conditions
-      "GROUP BY r.id")
-  Page<RecipeEntity> searchOrRecipesIdNotIn(@Param("level") LevelType level,
-      @Param("cookingTime") CookingTimeType cookingTime,
-      @Param("ingredients") List<String> ingredients,
-      @Param("ids") List<Long> ids,
-      Pageable pageable);
 
   void deleteAllByUser(UserEntity user);
 }
