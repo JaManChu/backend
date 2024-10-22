460289930193dfa374825d1ec0e75959efa796b6
jungheol <jungheol@naver.com>
2024-10-22 18:41:51 +0900

    Feat : RecipeRepository에서 Repository 어노테이션 제거

--- jamanchu/module-domain/src/main/java/com/recipe/jamanchu/domain/repository/RecipeRepository.java
+++ jamanchu/module-domain/src/main/java/com/recipe/jamanchu/domain/repository/RecipeRepository.java
@@ -11,9 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Long>, RecipeRepositoryCustom {
 
   // 평점순으로 레시피 가져오는 쿼리
