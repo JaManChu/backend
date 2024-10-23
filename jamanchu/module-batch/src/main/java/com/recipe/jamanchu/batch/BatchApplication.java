54a458fcc74628ef0cac2361ee775e1592710f34
heesane <tees3359@gmail.com>
2024-10-23 15:32:03 +0900

    feat : Batch Application 어노테이션 추가 - 패키지 객체 scan

--- jamanchu/module-batch/src/main/java/com/recipe/jamanchu/batch/BatchApplication.java
+++ jamanchu/module-batch/src/main/java/com/recipe/jamanchu/batch/BatchApplication.java
@@ -2,9 +2,15 @@ package com.recipe.jamanchu.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "com.recipe.jamanchu.domain.repository")
+@EntityScan(basePackages = "com.recipe.jamanchu.domain")
+@ComponentScan(basePackages = "com.recipe.jamanchu")
 @SpringBootApplication
 public class BatchApplication {
 
