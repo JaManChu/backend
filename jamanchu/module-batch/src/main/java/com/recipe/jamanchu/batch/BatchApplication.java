f69109cf3ef0387a845410d7bd1181894f57c3a5
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-10-25 00:39:19 +0900

    Feat : 어플리케이션 종료시 최종 작업 상태 로그 출력

--- jamanchu/module-batch/src/main/java/com/recipe/jamanchu/batch/BatchApplication.java
+++ jamanchu/module-batch/src/main/java/com/recipe/jamanchu/batch/BatchApplication.java
@@ -1,9 +1,13 @@
 package com.recipe.jamanchu.batch;
 
+import com.recipe.jamanchu.batch.batch.JobExecutionHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,9 +21,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BatchApplication {
 
 	public static void main(String[] args) {
-		int exit = SpringApplication.exit(SpringApplication.run(BatchApplication.class, args));
-		log.info("exit = {}", exit);
-		System.exit(exit);
+		ApplicationContext context = SpringApplication.run(BatchApplication.class, args);
+
+		JobExecutionHolder jobExecutionHolder = context.getBean(JobExecutionHolder.class);
+		JobExecution jobExecution = jobExecutionHolder.getJobExecution();
+
+		if (jobExecution != null) {
+			log.info("배치 작업이 종료되었습니다. 상태: {}", jobExecution.getStatus());
+			int exitCode = jobExecution.getStatus().isUnsuccessful() ? 1 : 0;
+			SpringApplication.exit(context, () -> exitCode);
+		} else {
+			log.warn("실행된 배치 작업을 찾을 수 없습니다.");
+			SpringApplication.exit(context, () -> 1);
+		}
 	}
 
 }
