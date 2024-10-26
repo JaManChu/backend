05d4a57208a639187b0795219b20f9e3bcdcf2fd
heesane <tees3359@gmail.com>
2024-10-26 18:32:38 +0900

    feat : Notify 객체 속성 추가

--- jamanchu/module-domain/src/main/java/com/recipe/jamanchu/domain/model/dto/response/notify/Notify.java
+++ jamanchu/module-domain/src/main/java/com/recipe/jamanchu/domain/model/dto/response/notify/Notify.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public class Notify {
 
+  private final Long recipeId;
+
   private final String recipeName;
 
   private final String message;
@@ -15,8 +17,8 @@ public class Notify {
 
   private final String commentUser;
 
-  public static Notify of(String recipeName, String message, Double rating, String commentUser) {
-    return new Notify(recipeName, message, rating, commentUser);
+  public static Notify of(Long recipeId, String recipeName, String message, Double rating, String commentUser) {
+    return new Notify(recipeId, recipeName, message, rating, commentUser);
   }
 
 }
