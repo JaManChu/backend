c6e6bccc9a63858cb69613c5ce94fa13bd970038
jungheol <jungheol@naver.com>
2024-10-28 22:05:35 +0900

    Feat : Entity 변수명 수정되면서 Repository 메서드명 및 쿼리 수정

--- jamanchu/module-domain/src/main/java/com/recipe/jamanchu/domain/repository/impl/RecipeRepositoryCustomImpl.java
+++ jamanchu/module-domain/src/main/java/com/recipe/jamanchu/domain/repository/impl/RecipeRepositoryCustomImpl.java
@@ -4,11 +4,11 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.recipe.jamanchu.domain.entity.QRecipeEntity;
+import com.recipe.jamanchu.domain.entity.QRecipeIngredientEntity;
 import com.recipe.jamanchu.domain.entity.RecipeEntity;
 import com.recipe.jamanchu.domain.model.dto.request.recipe.RecipesSearchDTO;
 import com.recipe.jamanchu.domain.repository.RecipeRepositoryCustom;
-import com.recipe.jamanchu.domain.entity.QRecipeEntity;
-import com.recipe.jamanchu.domain.entity.QRecipeIngredientEntity;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -39,34 +39,34 @@ public class RecipeRepositoryCustomImpl implements RecipeRepositoryCustom {
     if (recipesSearchDTO.getIngredients() != null && !recipesSearchDTO.getIngredients().isEmpty()) {
       for (String ingredient : recipesSearchDTO.getIngredients()) {
         combinedCondition = combinedCondition.or(
-            recipeIngredient.name.likeIgnoreCase("%" + ingredient + "%"));
+            recipeIngredient.riName.likeIgnoreCase("%" + ingredient + "%"));
       }
 
       // 모든 재료가 포함된 레시피를 찾아야 하므로, 각 재료를 포함하는 레시피 ID를 구합니다.
       for (String ingredient : recipesSearchDTO.getIngredients()) {
-        List<Long> ingredientRecipeIds = queryFactory.select(recipe.id)
+        List<Long> ingredientRecipeIds = queryFactory.select(recipe.rcpId)
             .from(recipe)
             .join(recipe.ingredients, recipeIngredient)
-            .where(recipeIngredient.name.likeIgnoreCase("%" + ingredient + "%"))
+            .where(recipeIngredient.riName.likeIgnoreCase("%" + ingredient + "%"))
             .fetch();
 
-        combinedCondition = combinedCondition.and(recipe.id.in(ingredientRecipeIds));
+        combinedCondition = combinedCondition.and(recipe.rcpId.in(ingredientRecipeIds));
       }
     }
 
     // 난이도 조건 추가
     if (recipesSearchDTO.getRecipeLevel() != null) {
-      combinedCondition = combinedCondition.and(recipe.level.eq(recipesSearchDTO.getRecipeLevel()));
+      combinedCondition = combinedCondition.and(recipe.rcpLevel.eq(recipesSearchDTO.getRecipeLevel()));
     }
 
     // 요리 시간 조건 추가
     if (recipesSearchDTO.getRecipeCookingTime() != null) {
-      combinedCondition = combinedCondition.and(recipe.time.eq(recipesSearchDTO.getRecipeCookingTime()));
+      combinedCondition = combinedCondition.and(recipe.rcpTime.eq(recipesSearchDTO.getRecipeCookingTime()));
     }
 
     // 스크랩된 레시피 제외 조건 추가
     if (!scrapedRecipeIds.isEmpty()) {
-      combinedCondition = combinedCondition.and(recipe.id.notIn(scrapedRecipeIds));
+      combinedCondition = combinedCondition.and(recipe.rcpId.notIn(scrapedRecipeIds));
     }
 
     // 모든 조건을 쿼리에 추가
@@ -100,7 +100,7 @@ public class RecipeRepositoryCustomImpl implements RecipeRepositoryCustom {
       BooleanExpression ingredientCondition = null;
 
       for (String ingredient : recipesSearchDTO.getIngredients()) {
-        BooleanExpression singleIngredientCondition = recipeIngredient.name.likeIgnoreCase("%" + ingredient + "%");
+        BooleanExpression singleIngredientCondition = recipeIngredient.riName.likeIgnoreCase("%" + ingredient + "%");
         ingredientCondition = (ingredientCondition == null) ? singleIngredientCondition : ingredientCondition.or(singleIngredientCondition);
       }
 
@@ -109,17 +109,17 @@ public class RecipeRepositoryCustomImpl implements RecipeRepositoryCustom {
 
     // 난이도 조건 추가
     if (recipesSearchDTO.getRecipeLevel() != null) {
-      combinedCondition = combinedCondition.or(recipe.level.eq(recipesSearchDTO.getRecipeLevel()));
+      combinedCondition = combinedCondition.or(recipe.rcpLevel.eq(recipesSearchDTO.getRecipeLevel()));
     }
 
     // 요리 시간 조건 추가
     if (recipesSearchDTO.getRecipeCookingTime() != null) {
-      combinedCondition = combinedCondition.or(recipe.time.eq(recipesSearchDTO.getRecipeCookingTime()));
+      combinedCondition = combinedCondition.or(recipe.rcpTime.eq(recipesSearchDTO.getRecipeCookingTime()));
     }
 
     // 스크랩된 레시피 제외 조건 추가
     if (!scrapedRecipeIds.isEmpty()) {
-      combinedCondition = combinedCondition.and(recipe.id.notIn(scrapedRecipeIds));
+      combinedCondition = combinedCondition.and(recipe.rcpId.notIn(scrapedRecipeIds));
     }
 
     // 모든 조건을 쿼리에 추가
